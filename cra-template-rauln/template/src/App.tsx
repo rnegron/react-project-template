6e25d5e0c6823dd37988d640af8fc7fdedfda33b
Raúl Negrón <raul.esteban.negron@gmail.com>
2020-06-07 16:22:59 -0400

    Fix and update Tailwind, use built-in purge

--- cra-template-rauln/template/src/App.tsx
+++ cra-template-rauln/template/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/tailwind.css'
 
 function App() {
   return <div></div>;
--- cra-template-rauln/template/tailwind.config.js
+++ cra-template-rauln/template/tailwind.config.js
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.tsx',
+  ],
   theme: {
     extend: {},
   },
